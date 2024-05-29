1c3d0cc5efc73c6f1e23cfcdc11504dd3a4a31c7
Astridgarciaa <astridfgarcia02@gmail.com>
2024-05-28 23:55:32 -0600

    Agregar archivos de proyecto.

---
+++ Hotel-Umg-Frontend/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Umg_Frontend.Models
+{
+    public class Usuario
+    {
+        
+        public int idUsuario { get; set; }
+
+        public string nombreUsuario { get; set; }
+
+        public string password { get; set; }
+
+        public int idEmpleado { get; set; }
+    }
+}
