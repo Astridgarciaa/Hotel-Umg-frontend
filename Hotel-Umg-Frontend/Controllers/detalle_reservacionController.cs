7bb73a6ccd2486a8d4fb1b1857773c7b892fc6db
Astridgarciaa <astridfgarcia02@gmail.com>
2024-05-30 23:07:50 -0600

    MVC Reservacion

---
+++ Hotel-Umg-Frontend/Controllers/detalle_reservacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Umg_Frontend.Controllers
+{
+    public class detalle_reservacionController : Controller
+    {
+        // GET: detalle_reservacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
