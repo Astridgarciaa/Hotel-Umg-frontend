29f4763b85ea3f733eb0a50adc6d278a2573f197
Josue-nets <mynormaldonado141@gmail.com>
2024-05-31 22:50:10 -0600

    Detalle reservacion medio corregido :(

---
+++ Hotel-Umg-Frontend/Models/Detalle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hotel_Umg_Frontend.Models
+{
+    public class Detalle
+    {
+        public int idDetalle { get; set; }
+
+        public int idReservacion { get; set; }
+        public virtual Reservacion Reservacion { get; set; }
+
+        public int idHabitacion { get; set; }
+        public virtual Habitacion Habitacion { get; set; }
+
+        public DateTime fechaReserva { get; set; }
+
+    }
+}
